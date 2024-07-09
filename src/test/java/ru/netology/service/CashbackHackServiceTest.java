c94bd05ce9d8b5ae9798d4b463fffb8c32cb19f1
Netotest <athomson@mail.ru>
2024-07-09 19:26:17 +0300

    Com 4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,40 +1,40 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
 
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void return1000For0() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected, "Actual bonuses not equals expected");
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void return1For999() {
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(actual, expected, "Actual bonuses not equals expected");
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void return999For1() {
         int amount = 1;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual, expected, "Actual bonuses not equals expected");
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void return0For1000() {
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected, "Actual bonuses not equals expected");
+        int expected = 1000;
+        assertEquals(actual, expected);
     }
 }
